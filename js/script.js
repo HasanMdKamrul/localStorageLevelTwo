b246f0ca5a0a40886beac4bcf1d4a393850a8155
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-06 22:40:33 +0200

    final counter commit added

--- js/script.js
+++ js/script.js
@@ -3,12 +3,24 @@
 // let count = localStorage.setItem('counter', 0);
 
 // ** get the saved value of counter from the ls
+const counterSet = ()=>{
+    localStorage.setItem('counter',0);
+}
 
 const savedCounterValue = localStorage.getItem('counter');
 let savedCounterValueParsed = JSON.parse(savedCounterValue);
 
 // ** Initial display
- savedCounterValueParsed > 0 ?  document.getElementById('display').innerText = `${savedCounterValueParsed}` : document.getElementById('display').innerText = `${0}`
+
+if (savedCounterValueParsed > 0) {
+    document.getElementById('display').innerText = `${savedCounterValueParsed}`
+} else{
+    counterSet();
+    const savedValue = JSON.parse(localStorage.getItem('counter'));
+    document.getElementById('display').innerText = `${savedValue}`
+}
+
+// savedCounterValueParsed > 0 ?  document.getElementById('display').innerText = `${savedCounterValueParsed}` :  counterSet();
 
 // ** counetr Increament
 
@@ -30,7 +42,7 @@ const decrement = ()=>{
     if(savedCounterValueParsed < 0){
         return
     }
-    // ** display after increament
+    // ** display after decrement
     document.getElementById('display').innerText = `${savedCounterValueParsed}`
     // ** stringyfy the new increament value and set to the ls
     
