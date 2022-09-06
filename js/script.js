738f25debcab35bddbecde8365787432f8a50b7c
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-06 22:16:44 +0200

    Increament and decreament button added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
 // ** Intial counter value saved in LS
 
-localStorage.setItem('counter', 0);
+// let count = localStorage.setItem('counter', 0);
+
+// ** get the saved value of counter from the ls
+
+const savedCounterValue = localStorage.getItem('counter');
+let savedCounterValueParsed = JSON.parse(savedCounterValue);
+
+// ** Initial display
+ savedCounterValueParsed > 0 ?  document.getElementById('display').innerText = `${savedCounterValueParsed}` : document.getElementById('display').innerText = `${0}`
 
 // ** counetr Increament
+
+const increament = ()=>{
+    ++savedCounterValueParsed;
+    // ** display after increament
+    document.getElementById('display').innerText = `${savedCounterValueParsed}`
+    // ** stringyfy the new increament value and set to the ls
+    
+    const savedCounterValueStringyfy = JSON.stringify(savedCounterValueParsed);
+
+    // ** set the value to ls
+
+    localStorage.setItem('counter', savedCounterValueStringyfy);
+   
+}
+const decrement = ()=>{
+    --savedCounterValueParsed;
+    if(savedCounterValueParsed < 0){
+        return
+    }
+    // ** display after increament
+    document.getElementById('display').innerText = `${savedCounterValueParsed}`
+    // ** stringyfy the new increament value and set to the ls
+    
+    const savedCounterValueStringyfy = JSON.stringify(savedCounterValueParsed);
+
+    // ** set the value to ls
+
+    localStorage.setItem('counter', savedCounterValueStringyfy);
+   
+}
+
